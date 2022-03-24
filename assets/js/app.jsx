583d076c493366861107306907c013bf1699243f
Brian Peiris <brianpeiris@gmail.com>
2022-03-24 11:47:54 -0400

    Remove existing frontend scaffold. Basic React app running.

---
+++ assets/js/app.jsx
@@ -0,0 +1,10 @@
+import "../css/app.css"
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return "hello";
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
