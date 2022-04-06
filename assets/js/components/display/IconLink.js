d35acfcab16356b14a954963c1adfdc8c5ea5382
Brian Peiris <brianpeiris@gmail.com>
2022-04-05 20:17:28 -0400

    Move IconLink css into separate file

--- assets/js/components/display/IconLink.js
+++ assets/js/components/display/IconLink.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
+import "../../../css/components/IconLink";
+
 export function IconLink(props) {
   return (
     <Link className="icon-link" to={props.to}>
