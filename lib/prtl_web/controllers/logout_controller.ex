21b51d09b6e634ba4c850f945f6e24187e3cfba5
Robin K Wilson <robin.kath.wilson@gmail.com>
2022-04-13 17:06:00 -0700

    hack to delete cookie off of myhub.net domain

--- lib/prtl_web/controllers/logout_controller.ex
+++ lib/prtl_web/controllers/logout_controller.ex
@@ -2,6 +2,9 @@ defmodule PrtlWeb.LogoutController do
   use PrtlWeb, :controller
 
   def index(conn, _) do
-    conn |> delete_resp_cookie(PrtlWeb.Plugs.Auth.get_cookie_name()) |> redirect(to: "/")
+    conn |> delete_resp_cookie(PrtlWeb.Plugs.Auth.get_cookie_name(), domain: get_cookie_domain(conn.host)) |> redirect(to: "/")
   end
+
+  defp get_cookie_domain("localhost"), do: ""
+  defp get_cookie_domain(_), do: ".myhubs.net"
 end
