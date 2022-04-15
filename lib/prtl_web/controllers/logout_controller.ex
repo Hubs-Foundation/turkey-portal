dc356b598e95f109dc1fe7c9da7285e29f46c78e
Robin K Wilson <robin.kath.wilson@gmail.com>
2022-04-14 17:31:34 -0700

    format

--- lib/prtl_web/controllers/logout_controller.ex
+++ lib/prtl_web/controllers/logout_controller.ex
@@ -2,6 +2,10 @@ defmodule PrtlWeb.LogoutController do
   use PrtlWeb, :controller
 
   def index(conn, _) do
-    conn |> delete_resp_cookie(PrtlWeb.Plugs.Auth.get_cookie_name(), domain: ".#{ conn.host |> String.split(".") |> Enum.take(-2) |> Enum.join(".") }") |> redirect(to: "/")
+    conn
+    |> delete_resp_cookie(PrtlWeb.Plugs.Auth.get_cookie_name(),
+      domain: ".#{conn.host |> String.split(".") |> Enum.take(-2) |> Enum.join(".")}"
+    )
+    |> redirect(to: "/")
   end
 end
