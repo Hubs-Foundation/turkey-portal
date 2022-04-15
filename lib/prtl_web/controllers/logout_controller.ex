9e88b712389e81809027b2a1f0ae3f1a4dd746c1
Robin K Wilson <robin.kath.wilson@gmail.com>
2022-04-14 17:15:14 -0700

    trying string manipulation for domain

--- lib/prtl_web/controllers/logout_controller.ex
+++ lib/prtl_web/controllers/logout_controller.ex
@@ -2,9 +2,6 @@ defmodule PrtlWeb.LogoutController do
   use PrtlWeb, :controller
 
   def index(conn, _) do
-    conn |> delete_resp_cookie(PrtlWeb.Plugs.Auth.get_cookie_name(), domain: get_cookie_domain(conn.host)) |> redirect(to: "/")
+    conn |> delete_resp_cookie(PrtlWeb.Plugs.Auth.get_cookie_name(), domain: ".#{ conn.host |> String.split(".") |> Enum.take(-2) |> Enum.join(".") }") |> redirect(to: "/")
   end
-
-  defp get_cookie_domain("localhost"), do: ""
-  defp get_cookie_domain(_), do: ".myhubs.net"
 end
