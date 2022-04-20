64bacf2035f0c268e44eda2b70509be6f90aed75
Brian Peiris <brianpeiris@gmail.com>
2022-04-20 19:08:55 -0400

    Add Orchestrator Client for creating hubs

---
+++ lib/prtl/orch_client.ex
@@ -0,0 +1,19 @@
+defmodule Prtl.OrchClient do
+  def create_hub(fxa_email, %Prtl.Hub{} = hub) do
+    orch_host = Application.get_env(:prtl, Prtl.OrchClient)[:orch_host]
+
+    orch_hub_create_params = %{
+      useremail: fxa_email,
+      hub_id: hub.instance_uuid,
+      subdomain: hub.subdomain,
+      tier: hub.tier,
+      ccu_limit: hub.ccu_limit,
+      storage_limit: hub.storage_limit_mb / 1024
+    }
+
+    HTTPoison.post(
+      "https://#{orch_host}/hc_instance",
+      Jason.encode!(orch_hub_create_params)
+    )
+  end
+end
