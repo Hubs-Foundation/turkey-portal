b60966cfc1db062ee77fb38fdab687980aa23c5c
Brian Peiris <brianpeiris@gmail.com>
2022-04-23 01:59:22 -0400

    Feature flag mechanism for backend and frontend

---
+++ assets/src/components/utils/feature-flags.js
@@ -0,0 +1,14 @@
+/* global global */
+const NODE_ENV = typeof global !== "undefined" && global.process.env.NODE_ENV;
+const FEATURE_FLAGS = window.FEATURE_FLAGS || {};
+
+const TIER_SELECTION = "tierSelection";
+const CCU_SELECTION = "ccuSelection";
+const STORAGE_SELECTION = "storageSelection";
+
+function featureIsEnabled(flag) {
+  if (NODE_ENV === "test") return true;
+  return !!FEATURE_FLAGS[flag];
+}
+
+export { featureIsEnabled, TIER_SELECTION, CCU_SELECTION, STORAGE_SELECTION };
