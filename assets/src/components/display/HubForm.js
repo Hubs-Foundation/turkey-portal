574ccfaee04eb36c408338cd09af7ef8b180f4ee
Brian Peiris <brianpeiris@gmail.com>
2022-05-25 17:13:16 -0400

    Form section subtitles and name validation

--- assets/src/components/display/HubForm.js
+++ assets/src/components/display/HubForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import "./HubForm.css";
@@ -7,6 +7,7 @@ import { IconDrive, IconUsers } from "../common/icons";
 import { formatMegabytes } from "../utils/formatNumber";
 
 export function HubForm({ hub, setHub, isSubmitting, onSubmit }) {
+  const [nameValidity, setNameValidity] = useState({ valid: true });
   const onFormSubmit = (e) => {
     e.preventDefault();
     onSubmit(hub);
@@ -14,19 +15,36 @@ export function HubForm({ hub, setHub, isSubmitting, onSubmit }) {
 
   const tierChoices = [
     { tier: "free", disabled: true, ccuLimit: 5, storageLimitMb: 250 },
-    { tier: "mvp", disabled: false, ccuLimit: 25, storageLimitMb: 2000 },
+    { tier: "mvp", disabled: false, ccuLimit: null, storageLimitMb: null },
   ];
 
   return (
     <div className="hub-form-container">
       <form className="hub-form" onSubmit={onFormSubmit}>
         <div>
-          <span className="form-section-title">Hub Name</span>
-          <input type="text" value={hub.name} onChange={(e) => setHub({ ...hub, name: e.target.value })} />
+          <span className="form-section-title">Hub Nickname</span>
+          {nameValidity.valid || nameValidity.valueMissing ? (
+            <span className="form-section-subtitle">For use within the dashboard area only</span>
+          ) : (
+            nameValidity.patternMismatch && (
+              <span className="form-section-subtitle invalid">Hub name too long (24 characters max)</span>
+            )
+          )}
+          <input
+            type="text"
+            value={hub.name}
+            required
+            pattern=".{1,24}"
+            onChange={(e) => {
+              setNameValidity(e.target.validity);
+              setHub({ ...hub, name: e.target.value });
+            }}
+          />
         </div>
 
         <div>
           <span className="form-section-title">Hub Tier</span>
+          <span className="form-section-subtitle">Hub tiers vary in visitor capacity and asset storage space</span>
           {tierChoices.map((tierChoice) => (
             <label
               key={tierChoice.tier}
@@ -46,7 +64,7 @@ export function HubForm({ hub, setHub, isSubmitting, onSubmit }) {
               </div>
               <div>
                 <IconUsers />
-                <span>{tierChoice.ccuLimit}</span>
+                <span>{tierChoice.ccuLimit || "-"}</span>
               </div>
               <div>
                 <IconDrive />
@@ -57,10 +75,13 @@ export function HubForm({ hub, setHub, isSubmitting, onSubmit }) {
         </div>
 
         <div className="web-address">
-          <span className="form-section-title">Web Address (URL)</span>
-          <input type="text" value={hub.subdomain} onChange={(e) => setHub({ ...hub, subdomain: e.target.value })} />
-          <span className="form-section-subtitle">Preview</span>
+          <div className="web-address-header">
+            <span className="form-section-title">Web Address (URL)</span>
+            <span className="form-section-subtitle">Supports letters (a to z), digits (0 to 9), and hyphens (-)</span>
+          </div>
           <div>
+            <input type="text" value={hub.subdomain} onChange={(e) => setHub({ ...hub, subdomain: e.target.value })} />
+            &nbsp;
             <span className="domain">
               <span className="subdomain">{hub.subdomain}</span>.myhubs.net
             </span>
@@ -82,9 +103,9 @@ export function HubForm({ hub, setHub, isSubmitting, onSubmit }) {
         <span>Tier</span>
         <span className={`tag ${hub.tier}`}>{hub.tier}</span>
         <span>People</span>
-        <span>{hub.ccuLimit}</span>
+        <span className="hub-form-summary-value">Unlimited</span>
         <span>Capacity</span>
-        <span>{formatMegabytes(hub.storageLimitMb)}</span>
+        <span className="hub-form-summary-value">Unlimited</span>
       </div>
     </div>
   );
