813c4f9ff44a9e2b7ee39ce4d810ee77471a267a
Nick Grato <nickgrato@ngrato-27545.local>
2022-06-02 11:34:10 -0700

    updating PR prittier etc

--- client/components/cards/SkeletonCard/SkeletonCard.tsx
+++ client/components/cards/SkeletonCard/SkeletonCard.tsx
@@ -7,24 +7,30 @@ type SkeletonCardPropsT = {
   qty: number
 }
 
-
 /**
- * Skeleton Card: This card is used to create faux loading ui of widgets and rows. Stack 
+ * Skeleton Card: This card is used to create faux loading ui of widgets and rows. Stack
  * the skeleton cards to build a custom grid of widgets to mimic the actual UI.
  */
 
-
 const SkeletonCard = ({ category, qty = 1 }: SkeletonCardPropsT) => {
   return (
-    <div className={`${styles.card_wrapper} ${category === 'square' ? styles.square_wrapper : styles.row_wrapper}`}>
-      {
-        [...Array(qty)].map((arr, i) => {
-          return <div key={i} className={`${styles.card} pulse ${category === 'square' ? styles.square : styles.row}`}></div>
-        })
-      }
+    <div
+      className={`${styles.card_wrapper} ${
+        category === 'square' ? styles.square_wrapper : styles.row_wrapper
+      }`}
+    >
+      {[...Array(qty)].map((arr, i) => {
+        return (
+          <div
+            key={i}
+            className={`${styles.card} pulse ${
+              category === 'square' ? styles.square : styles.row
+            }`}
+          ></div>
+        )
+      })}
     </div>
   )
 }
-
 
 export default SkeletonCard
