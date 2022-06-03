9b261246d34af06b46cdc722496fdb79f25802a7
Nick Grato <nickgrato@ngrato-27545.local>
2022-06-03 11:49:32 -0700

    adding logout functionality

--- client/components/navigation/MainNav/MainNav.tsx
+++ client/components/navigation/MainNav/MainNav.tsx
@@ -1,24 +1,34 @@
-import { useRef } from 'react'
-import styles from './MainNav.module.scss'
-import Link from 'next/link'
-import Image from 'next/image'
-import { selectAccount } from 'store/accountSlice'
-import { useSelector } from 'react-redux'
-import Dropdown, { dropdownT } from '@Shared/Dropdown/Dropdown'
-import ExternalLink from '@Shared/ExternalLink/ExternalLink'
-import InternalLink from '@Shared/InternalLink/InternalLink'
+import { useRef, useCallback } from 'react';
+import { useRouter } from 'next/router';
+import styles from './MainNav.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
+import { logOut } from 'services/account.service';
+import { selectAccount } from 'store/accountSlice';
+import { useSelector } from 'react-redux';
+import Dropdown, { dropdownT } from '@Shared/Dropdown/Dropdown';
+import ExternalLink from '@Shared/ExternalLink/ExternalLink';
+import Button from '@Shared/Button/Button';
 
 type MainNavPropsT = {
-  classProp?: string
-}
+  classProp?: string;
+};
 
 const MainNav = ({ classProp = '' }: MainNavPropsT) => {
+  const account = useSelector(selectAccount);
+  const dropdownRef = useRef<dropdownT>(null);
+  const router = useRouter();
 
-  const account = useSelector(selectAccount)
-  const dropdownRef = useRef<dropdownT>(null)
+  const onlogOutClick = useCallback(async () => {
+    dropdownRef.current?.closeDropdown();
+    await logOut();
+    router.push({
+      pathname: '/login',
+    });
+  }, [router]);
 
   /**
-   * Dropdown Content 
+   * Dropdown Content
    */
   const DropdownContent = (
     <>
@@ -35,32 +45,25 @@ const MainNav = ({ classProp = '' }: MainNavPropsT) => {
           <ExternalLink
             classProp={styles.account_manage_link}
             icon="external-link"
-            target='_blank'
+            target="_blank"
             href={'#'}
             onClick={() => {
-              dropdownRef.current?.closeDropdown()
-            }}>
+              dropdownRef.current?.closeDropdown();
+            }}
+          >
             Manage your Firefox Account
           </ExternalLink>
         </div>
       </div>
 
-      <hr className='dropdown-hr' />
+      <hr className="dropdown-hr" />
 
       {/* Sign Out  */}
       <div className={`padding-10 ${styles.account_details_wrapper}`}>
-        <InternalLink
-          href="/login"
-          icon='log-out'
-          onClick={() => {
-            dropdownRef.current?.closeDropdown()
-          }}
-        >Sign Out
-        </InternalLink>
+        <Button icon="log-out" text="Sign Out" onClick={onlogOutClick} />
       </div>
     </>
-  )
-
+  );
 
   /**
    * Main Nav JSX
@@ -68,7 +71,6 @@ const MainNav = ({ classProp = '' }: MainNavPropsT) => {
   return (
     <div className={`${styles.main_nav_wrapper} ${classProp}`}>
       <div className={styles.main_nav_container}>
-
         {/* Main navigation links / logo */}
         <div className={styles.main_nav_contents}>
           {/* Logo */}
@@ -84,19 +86,24 @@ const MainNav = ({ classProp = '' }: MainNavPropsT) => {
         {/* Account information  */}
         <Dropdown
           ref={dropdownRef}
-          alignment='right'
-          cta={(
+          alignment="right"
+          cta={
             <div className={styles.main_nav_account}>
               {account.profilePic && (
-                <Image alt="profile picture" width="50" height="50" src={account.profilePic} />
+                <Image
+                  alt="profile picture"
+                  width="50"
+                  height="50"
+                  src={account.profilePic}
+                />
               )}
-            </div>)}
+            </div>
+          }
           content={DropdownContent}
         />
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainNav
+export default MainNav;
--- client/components/shared/ExternalLink/ExternalLink.tsx
+++ client/components/shared/ExternalLink/ExternalLink.tsx
@@ -1,16 +1,16 @@
-import { ReactNode, HTMLAttributeAnchorTarget } from 'react'
-import styles from './ExternalLink.module.scss'
-import { IconT } from 'types/General'
-import Icon from '@Shared/Icon/Icon'
+import { ReactNode, HTMLAttributeAnchorTarget } from 'react';
+import styles from './ExternalLink.module.scss';
+import { IconT } from 'types/General';
+import Icon from '@Shared/Icon/Icon';
 
 type ExternalLinkProps = {
-  href: string
-  classProp?: string,
-  children?: ReactNode,
-  icon?: IconT,
-  target?: HTMLAttributeAnchorTarget,
-  onClick?: Function
-}
+  href: string;
+  classProp?: string;
+  children?: ReactNode;
+  icon?: IconT;
+  target?: HTMLAttributeAnchorTarget;
+  onClick?: Function;
+};
 
 const ExternalLink = ({
   href,
@@ -18,15 +18,21 @@ const ExternalLink = ({
   children,
   icon,
   target = '_blank',
-  onClick
+  onClick,
 }: ExternalLinkProps) => {
-
   return (
-    <a href={href} target={target} className={`${classProp} ${styles.link}`} onClick={(e) => { onClick && onClick(e) }}>
+    <a
+      href={href}
+      target={target}
+      className={`${classProp} ${styles.link}`}
+      onClick={(e) => {
+        onClick && onClick(e);
+      }}
+    >
       {children}
       {icon && <Icon name={icon} color="currentColor" size={16} />}
     </a>
-  )
-}
+  );
+};
 
-export default ExternalLink
+export default ExternalLink;
