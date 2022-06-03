203bac68b04ed059510ea8b3da94ce63cd476499
Nick Grato <nickgrato@ngrato-27545.local>
2022-06-03 15:00:15 -0700

    adding side nav placeholder

--- client/layouts/MainLayout/MainLayout.tsx
+++ client/layouts/MainLayout/MainLayout.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import MainNav from '@Navigation/MainNav/MainNav'
+import React from 'react';
+import MainNav from '@Navigation/MainNav/MainNav';
+import SideNav from '@Navigation/SideNav/SideNav';
+import styles from './MainLayout.module.scss';
 
 type MainLayoutPropsT = {
-  children: React.ReactNode,
-}
+  children: React.ReactNode;
+};
 
 const MainLayout = ({ children }: MainLayoutPropsT) => {
   return (
     <div>
       <MainNav />
-      {children}
+      <section className={styles.page_wrapper}>
+        <SideNav />
+        {children}
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
