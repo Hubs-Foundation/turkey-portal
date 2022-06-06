b499a76fddc84f01d33fbe260fd2d6f6456b84ec
Nick Grato <nickgrato@ngrato-27545.local>
2022-06-06 10:14:44 -0700

    spelling

--- client/hooks/useMediaQuery.tsx
+++ client/hooks/useMediaQuery.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-// Base MediaQuery Hooke
+// Base MediaQuery Hook
 const useMediaQuery = (query: string) => {
   const [matches, setMatches] = useState(false);
 
