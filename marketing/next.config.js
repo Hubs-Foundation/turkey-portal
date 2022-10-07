c186cfa9e279208627131733738f00e84c008ca8
Geng Tan <tanfarming@hotmail.com>
2022-10-07 12:47:17 -0400

    word

--- marketing/next.config.js
+++ marketing/next.config.js
@@ -13,6 +13,15 @@ const nextConfig = {
   },
 };
 
+
+console.log("process.env.AUTH_SERVER: ", process.env.AUTH_SERVER)
+console.log("process.env.FXA_SERVER: ", process.env.FXA_SERVER)
+console.log("process.env.HUB_ROOT_DOMAIN: ", process.env.HUB_ROOT_DOMAIN)
+console.log("process.env.FXA_PAYMENT_URL: ", process.env.FXA_PAYMENT_URL)
+console.log("process.env.DASH_ROOT_DOMAIN: ", process.env.DASH_ROOT_DOMAIN)
+console.log("process.env.PRODUCT_ID: ", process.env.PRODUCT_ID)
+console.log("process.env.PLAN_ID_EA: ", process.env.PLAN_ID_EA)
+
 console.log("nextConfig: ", nextConfig)
 
 module.exports = nextConfig;
