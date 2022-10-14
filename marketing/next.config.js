9cbf53cc16fc399171f7357fc2186b1cc644a11d
Nick Grato <nickgrato@ngrato-27545.local>
2022-10-14 08:28:57 -0700

    updating code:

--- marketing/next.config.js
+++ marketing/next.config.js
@@ -1,30 +1,39 @@
 /** @type {import('next').NextConfig} */
 
-if (process.env.ENV == 'prod') {
-  console.log('if', process.env.ENV);
-  FXA_SERVER = 'accounts.firefox.com';
-  AUTH_SERVER = 'XXXXauth.dev.myhubs.net';
-  HUB_ROOT_DOMAIN = 'TBD333';
-  DASH_ROOT_DOMAIN = 'dashboard.dev.myhubs.net';
-  FXA_PAYMENT_URL = 'TBD555';
-  PRODUCT_ID = 'TBD666';
-  PLAN_ID_EA = 'TBD777';
-} else {
-  console.log('else', process.env.ENV);
-  FXA_SERVER = 'accounts.firefox.com';
-  AUTH_SERVER = 'auth.dev.myhubs.net';
-  HUB_ROOT_DOMAIN = 'dev.myhubs.net';
-  DASH_ROOT_DOMAIN = 'dashboard.dev.myhubs.net';
-  FXA_PAYMENT_URL = 'https://payments-stage.fxa.nonprod.cloudops.mozgcp.net';
-  PRODUCT_ID = 'prod_KPReWHqwGqZBzc';
-  PLAN_ID_EA = 'price_1Jkcl3Kb9q6OnNsLFbECmMtd';
-}
+// if (process.env.ENV == 'prod') {
+//   console.log('if', process.env.ENV);
+//   FXA_SERVER = 'accounts.firefox.com';
+//   AUTH_SERVER = 'XXXXauth.dev.myhubs.net';
+//   HUB_ROOT_DOMAIN = 'TBD333';
+//   DASH_ROOT_DOMAIN = 'dashboard.dev.myhubs.net';
+//   FXA_PAYMENT_URL = 'TBD555';
+//   PRODUCT_ID = 'TBD666';
+//   PLAN_ID_EA = 'TBD777';
+// } else {
+//   // console.log('else', process.env.ENV);
+//   // FXA_SERVER = 'accounts.firefox.com';
+//   // AUTH_SERVER = 'auth.dev.myhubs.net';
+//   // HUB_ROOT_DOMAIN = 'dev.myhubs.net';
+//   // DASH_ROOT_DOMAIN = 'dashboard.dev.myhubs.net';
+//   // FXA_PAYMENT_URL = 'https://payments-stage.fxa.nonprod.cloudops.mozgcp.net';
+//   // PRODUCT_ID = 'prod_KPReWHqwGqZBzc';
+//   // PLAN_ID_EA = 'price_1Jkcl3Kb9q6OnNsLFbECmMtd';
+// }
+
+FXA_SERVER = 'accounts.firefox.com';
+AUTH_SERVER = 'XXXXauth.dev.myhubs.net';
+HUB_ROOT_DOMAIN = 'TBD333';
+DASH_ROOT_DOMAIN = 'dashboard.dev.myhubs.net';
+FXA_PAYMENT_URL = 'TBD555';
+PRODUCT_ID = 'TBD666';
+PLAN_ID_EA = 'TBD777';
+PROD = process.env.ENV;
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   publicRuntimeConfig: {
-    AUTH_SERVER: process.env.AUTH_SERVER,
+    AUTH_SERVER: AUTH_SERVER,
     FXA_SERVER: FXA_SERVER,
     HUB_ROOT_DOMAIN: HUB_ROOT_DOMAIN,
     FXA_PAYMENT_URL: FXA_PAYMENT_URL,
