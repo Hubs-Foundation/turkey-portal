b39177486dd63dba3317ed2aa3e8e8c1264afdab
Nick Grato <nickgrato@ngrato-27545.local>
2022-11-15 12:32:12 -0800

    adding the right currency display

--- marketing/types/Countries.ts
+++ marketing/types/Countries.ts
@@ -1,3 +1,14 @@
 export enum CountriesE {
   GERMANY = 'DE',
 }
+
+export const CountrieCurrency = {
+  DE: {
+    abbrev: 'Euro',
+    symbol: '€',
+  },
+  US: {
+    abbrev: 'USD',
+    symbol: '$',
+  },
+};
