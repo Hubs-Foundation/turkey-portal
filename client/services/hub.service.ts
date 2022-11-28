45cad33fd95cd4c3c767d03d591294efd6309ce1
Nick Grato <nickgrato@ngrato-27545.local>
2022-11-28 13:32:21 -0800

    removing timeout

--- client/services/hub.service.ts
+++ client/services/hub.service.ts
@@ -11,7 +11,6 @@ export const getHubs = async () => {
   return axios
     .get(`${PUBLIC_API_SERVER}${API_PATH}`, {
       withCredentials: true,
-      timeout: 300_000,
     })
     .then((response) => {
       return response.data;
