505898552275d77cd939e9e1d55b878273922232
Nick Grato <nickgrato@ngrato-27545.local>
2022-12-13 13:23:58 -0800

    adding a date picker

--- client/pages/participant-sorter/index.tsx
+++ client/pages/participant-sorter/index.tsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import type { GetServerSidePropsContext } from 'next';
 import styles from './participant-sorter.module.scss';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 type ParticipantSorterTPropsT = {};
 
 const ParticipantSorter = ({}: ParticipantSorterTPropsT) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+
+  const onChange = (dates: any) => {
+    console.log('datres', dates);
+    const [start, end] = dates;
+    console.log('start', start);
+    setStartDate(start);
+    setEndDate(end);
+  };
+
   return (
     <div className="page_wrapper">
       <Head>
@@ -14,6 +28,14 @@ const ParticipantSorter = ({}: ParticipantSorterTPropsT) => {
 
       <main>
         <h1>sorter</h1>
+
+        <DatePicker
+          selected={startDate}
+          onChange={onChange}
+          startDate={startDate}
+          endDate={endDate}
+          selectsRange
+        />
       </main>
     </div>
   );
