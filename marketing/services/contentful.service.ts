9fb276cc0ad3db1ce905e5502e19d126c8a54dda
Nick Grato <nickgrato@ngrato-27545.local>
2023-03-22 17:49:29 -0700

    testing string

--- marketing/services/contentful.service.ts
+++ marketing/services/contentful.service.ts
@@ -16,7 +16,7 @@ const URL = `${BASE_URL}${SPACE}`;
 const PROTOCOLS = {
   headers: {
     'content-type': 'application/json',
-    authorization: `Bearer 2Ou5lQVJKiR_0_UZn4i_Ro2TRH5hN82UnP93Vn5HGt8`,
+    authorization: `Bearer ${process.env.CONTENTFUL_TOKEN}`,
   },
 };
 
@@ -29,7 +29,7 @@ const PROTOCOLS = {
  */
 const client = createClient({
   space: SPACE,
-  accessToken: '2Ou5lQVJKiR_0_UZn4i_Ro2TRH5hN82UnP93Vn5HGt8',
+  accessToken: `${process.env.CONTENTFUL_TOKEN}` ?? '',
 });
 
 /**
