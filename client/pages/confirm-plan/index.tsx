def6734f782ae378f0ba4bb0b4c429bc6c5f30e2
Robin Wilson <robin.kath.wilson@gmail.com>
2023-04-17 22:46:26 -0700

    centered modal into middle of screen and worked on responsiveness

--- client/pages/confirm-plan/index.tsx
+++ client/pages/confirm-plan/index.tsx
@@ -2,7 +2,7 @@ import SkeletonCard from '@Cards/SkeletonCard/SkeletonCard';
 import ConfirmPlanModel from 'components/Modals/ConfirmPlanModal/ConfirmPlanModel';
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import { pageRequireAuthentication } from 'services/routeGuard.service';
-import styles from '../dashboard/dashboard.module.scss';
+import styles from './confirm-plan.module.scss';
 import { enabledStarterPlan } from 'util/featureFlag';
 import { redirectToDashboard } from 'util/redirects';
 import { useEffect } from 'react';
@@ -16,9 +16,16 @@ const ConfirmPlan = () => {
   }, []);
   return (
     <div className="page_wrapper">
-      <div className={styles.cards_wrapper}>
-        <SkeletonCard qty={2} category="square" pulse={false} />
-        <SkeletonCard qty={1} category="row" pulse={false} />
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <SkeletonCard qty={2} category="square" pulse={false} />
+          <SkeletonCard
+            qty={1}
+            category="row"
+            pulse={false}
+            classProp={styles.card}
+          />
+        </div>
       </div>
 
       <ConfirmPlanModel />
