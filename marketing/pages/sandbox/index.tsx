053be97d65381d817724b11ae63f359cd2b48dd7
Nick Grato <nickgrato@ngrato-27545.local>
2023-05-08 17:21:46 -0700

    cleaning up sandbox

--- marketing/pages/sandbox/index.tsx
+++ marketing/pages/sandbox/index.tsx
@@ -1,44 +1,19 @@
-import { useState } from 'react';
 import Head from 'next/head';
-import Modal from '@Shared/Modal/Modal';
-import { Button } from '@mozilla/lilypad-ui';
-import ContactFormModal from 'components/modals/ContactFormModal/ContactFormModal';
 
 type SandboxPropsT = {};
 
+/**
+ * This modal is used to sandbox code. feel free to play, this will
+ * not show up on prod
+ */
 const Sandbox = ({}: SandboxPropsT) => {
-  const [showModal, setShowModal] = useState<Boolean>(false);
-
-  /**
-   * Close Modal
-   */
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
-  /**
-   * Open Modal
-   */
-  const handleOpenModal = () => {
-    setShowModal(true);
-  };
-
   return (
     <div className="page_wrapper">
       <Head>
         <title>Sandbox</title>
       </Head>
       <main>
-        sand box
-        <Button
-          label="modal"
-          text="open modal"
-          onClick={handleOpenModal}
-          classProp="mt-100"
-        />
-        <Modal onClose={handleCloseModal} isVisible={showModal}>
-          <ContactFormModal onClose={handleCloseModal} />
-        </Modal>
+        <h1>Sandbox</h1>
       </main>
     </div>
   );
