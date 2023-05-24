17e299c5264602ff2aa8473949f96b10ecf4718a
Nick Grato <nickgrato@ngrato-27545.local>
2023-05-24 15:46:14 -0700

    forgot these files

--- marketing/components/shared/Subscribe/Subscribe.tsx
+++ marketing/components/shared/Subscribe/Subscribe.tsx
@@ -3,7 +3,6 @@ import StandardPlanCard from './StandardPlanCard/StandardPlanCard';
 import StarterPlanCard from './StarterPlanCard/StarterPlanCard';
 import BusinessPlanCard from './BusinessPlanCard/BusinessPlanCard';
 import styles from './Subscribe.module.scss';
-import { enabledStarterPlan } from 'util/utilities';
 import Swoosh from '@Shared/Swoosh/Swoosh';
 
 type SubscribePropsT = {
@@ -17,33 +16,9 @@ const Subscribe = ({ classProp = '' }: SubscribePropsT) => {
         <Swoosh location="top" />
         <div className={styles.container}>
           <div className={styles.cards}>
-            {enabledStarterPlan() ? (
-              <>
-                <StarterPlanCard />
-                <StandardPlanCard />
-                <BusinessPlanCard />
-              </>
-            ) : (
-              <>
-                {/* TRY IT OUT  */}
-                <SubCard
-                  title="Explore Hubs"
-                  classProp={styles.card_one}
-                  cta="Try Mozilla's Hub"
-                  ctaUrl="/Pvg5MMt/hubs-demo"
-                >
-                  <p>See the world in 3D and share it with your friends.</p>
-                  <p>Try out a variety of avatars.</p>
-                  <p>Tour incredible community Hubs.</p>
-                </SubCard>
-
-                {/* SUBSCRIBE  */}
-                <StandardPlanCard />
-
-                {/* BUSINESS TIER  */}
-                <BusinessPlanCard />
-              </>
-            )}
+            <StarterPlanCard />
+            <StandardPlanCard />
+            <BusinessPlanCard />
           </div>
         </div>
         <Swoosh location="bottom" />
