f0a283be58584ae5a347b4da3a26a7f490949e3e
tanfarming <tanfarming@hotmail.com>
2023-06-17 15:15:03 -0400

    word

--- priv/repo/migrations/20230616000001_add_domain_and_region_columns_to_hubs.exs
+++ priv/repo/migrations/20230616000001_add_domain_and_region_columns_to_hubs.exs
@@ -3,8 +3,8 @@ defmodule Dash.Repo.Migrations.AddDomainAndRegionColumnsToHubs do
 
   def change do
     alter table(:hubs) do
-      add :domain :string
-      add :region :string
+      add :domain, :string
+      add :region, :string
     end
   end
 end
