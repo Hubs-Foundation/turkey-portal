826d9572743fc462e9c88e0d75ebd78a7baa4e8b
tanfarming <tanfarming@hotmail.com>
2023-06-17 14:45:59 -0400

    word

--- priv/repo/migrations/20230616000000_add_domain_region_columns_to_hubs.exs
+++ priv/repo/migrations/20230616000000_add_domain_region_columns_to_hubs.exs
@@ -1,35 +1,10 @@
 defmodule Dash.Repo.Migrations.AddDomainRegionColumnsToHubs do
   use Ecto.Migration
 
-  def up do
-    execute("""
-      DO $$ 
-      BEGIN
-          IF NOT EXISTS(
-            SELECT * FROM information_schema.columns
-            WHERE table_name = 'hubs' AND column_name = 'domain'
-          ) THEN
-              ALTER TABLE hubs ADD COLUMN domain VARCHAR(255);
-          END IF;
-      END
-      $$;
-    """)
-    execute("""
-      DO $$ 
-      BEGIN
-          IF NOT EXISTS(
-            SELECT * FROM information_schema.columns
-            WHERE table_name = 'hubs' AND column_name = 'region'
-          ) THEN
-              ALTER TABLE hubs ADD COLUMN region VARCHAR(255);
-          END IF;
-      END
-      $$;
-    """)
-  end
-
-  def down do
-    execute("ALTER TABLE hubs DROP COLUMN IF EXISTS domain")
-    execute("ALTER TABLE hubs DROP COLUMN IF EXISTS region")
+  def change do
+    alter table(:hubs) do
+      add :domain
+      add :region      
+    end
   end
 end
