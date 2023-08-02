82391611a72da9b0cb4b214899fd12e9307d3b0b
Nick Grato <nickgrato@ngrato-27545.local>
2023-08-02 16:06:34 -0700

    api error handle

--- client/pages/api/plans.ts
+++ client/pages/api/plans.ts
@@ -3,18 +3,17 @@ import { AcceptedRegionCodeT, ACCEPTED_REGION_CODES } from 'types/Countries';
 import { PLAN_ID_MAP } from './plan.cont';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Default United States
-  let regionCode: AcceptedRegionCodeT = 'US';
-
   if (!('region' in req.query) || req.query.region === '') {
     res.status(404).json({ error: 'Region query param not found.' });
   }
-  if (
-    !ACCEPTED_REGION_CODES.includes(req.query.region as AcceptedRegionCodeT)
-  ) {
+
+  const regionQuery = (
+    req.query.region as string
+  ).toUpperCase() as AcceptedRegionCodeT;
+
+  if (!ACCEPTED_REGION_CODES.includes(regionQuery)) {
     res.status(404).json({ error: 'Region not found' });
   }
 
-  regionCode = req.query.region as AcceptedRegionCodeT;
-  res.status(200).json(PLAN_ID_MAP[regionCode]);
+  res.status(200).json(PLAN_ID_MAP[regionQuery]);
 }
