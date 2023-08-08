114d3601943d8e5842af080fcaa20a929251cfd8
Nick Grato <nickgrato@ngrato-27545.local>
2023-08-08 15:13:58 -0700

    new pages:

---
+++ marketing/pages/blog/[slug].tsx
@@ -0,0 +1,58 @@
+import { CustomSectionsT } from 'types';
+import Custom from '@Shared/Custom/Custom';
+import {
+  getCustomPageData,
+  getStaticPathEntries,
+} from 'services/contentful.service';
+
+type CustomPagePropsT = {
+  sectionsData: CustomSectionsT;
+};
+
+type GetStaticPropsT = {
+  params: {
+    slug: string;
+  };
+};
+
+const CustomPage = () => {
+  return (
+    <div className="page_wrapper">
+      <main>i'm a slug page.</main>
+    </div>
+  );
+};
+
+export default CustomPage;
+
+// export async function getStaticProps({ params }: GetStaticPropsT) {
+//   try {
+//     const sectionsData = await getCustomPageData(params.slug);
+
+//     return {
+//       props: {
+//         sectionsData,
+//       },
+//     };
+//   } catch {
+//     return {
+//       props: {
+//         sectionsData: [],
+//       },
+//     };
+//   }
+// }
+
+// export async function getStaticPaths() {
+//   // Get Entries
+//   const entries = await getStaticPathEntries('customPage');
+//   // Create Paths Object
+//   const paths = entries.items.map((item) => {
+//     return { params: { slug: item.fields.slug } };
+//   });
+
+//   return {
+//     paths: paths,
+//     fallback: 'blocking',
+//   };
+// }
