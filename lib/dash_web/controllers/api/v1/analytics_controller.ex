6a377980ea03787ef55431360d562c7c7856d14f
Nick Grato <nickgrato@ngrato-27545.local>
2023-11-02 08:00:02 -0700

    formatting

--- lib/dash_web/controllers/api/v1/analytics_controller.ex
+++ lib/dash_web/controllers/api/v1/analytics_controller.ex
@@ -3,7 +3,7 @@ defmodule DashWeb.Api.V1.AnalyticsController do
 
   require Logger
 
-  def show(conn, %{"start_date" => start_date, "end_date" => end_date} = params) do
+  def show(conn, %{"start_date" => start_date, "end_date" => end_date} = _params) do
     hubs = Dash.get_hubs_by_date(start_date, end_date)
 
     conn
